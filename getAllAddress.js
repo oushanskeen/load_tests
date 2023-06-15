6539ba014bad9818a036c08ea78cba46a016fba6
oushanskeen <inta.soul@gmail.com>
2023-06-15 16:30:08 +0200

    Add charts for response status

--- getAllAddress.js
+++ getAllAddress.js
@@ -17,24 +17,24 @@ const txsAmount = 100
 const blockTxsRequest = async (txsAmount,chainId) => {
 
     const response =
-    await fetch("https://explorer-graph-prod.dexguru.biz/graphql", {
-        "credentials": "omit",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "content-type": "application/json",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "cross-site"
-        },
-        "referrer": "https://canto.dex.guru/",
-        "body": "{\"operationName\":\"Transactions\",\"variables\":{},\"query\":\"query Transactions {\\n  transactions(first: " + txsAmount + ", chainId: " + chainId + ") {\\n    hash\\n    blockHash\\n    blockNumber\\n    blockTimestamp\\n    fromAddress\\n    toAddress\\n    value\\n    __typename\\n  }\\n}\"}",
-        "method": "POST",
-        "mode": "cors"
-    });
-    const data = await response.json()
-    console.log("/getAllAddress.js/blockTxsRequest data: ", data.data)
+        await fetch("https://explorer-graph-prod.dexguru.biz/graphql", {
+            "credentials": "omit",
+            "headers": {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+                "Accept": "*/*",
+                "Accept-Language": "en-US,en;q=0.5",
+                "content-type": "application/json",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "cross-site"
+            },
+            "referrer": "https://canto.dex.guru/",
+            "body": "{\"operationName\":\"Transactions\",\"variables\":{},\"query\":\"query Transactions {\\n  transactions(first: " + txsAmount + ", chainId: " + chainId + ") {\\n    hash\\n    blockHash\\n    blockNumber\\n    blockTimestamp\\n    fromAddress\\n    toAddress\\n    value\\n    __typename\\n  }\\n}\"}",
+            "method": "POST",
+            "mode": "cors"
+        });
+        const data = await response.json()
+        // console.log("/getAllAddress.js/blockTxsRequest data: ", data.data)
 
     return data.data
 }
@@ -64,11 +64,37 @@ const f = async () => {
 
         acc[chain].txs = [... new Set(acc[chain].txs)]
         acc[chain].adresses = [... new Set(acc[chain].adresses)]
+        acc[chain].blockNumbers.push([... new Set(acc[chain].blockNumbers)])
         // console.log("Acc: ", acc)
     }
     return acc
 }
 const pool = f()
+
+const getAddressType = async (address) => {
+
+    const response = await fetch("https://explorer-graph-stage.dexguru.biz/graphql", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site"
+    },
+    "body": "{\"query\":\"query MyQuery {\\n  address(address: \\\"" + address + "\\\") {\\n    name\\n    address\\n    addressType\\n  }\\n}\",\"operationName\":\"MyQuery\"}",
+    "method": "POST",
+    "mode": "cors"
+});
+
+    const data = await response.json()
+    // console.log("Address type: ", data)
+    // console.log("Address type: ", data.data)
+    return data.data
+}
 
 let urls = []
 const makeUrls = async (p) => {
@@ -78,14 +104,34 @@ const makeUrls = async (p) => {
     let adresses = pool[chain].adresses
     let txs = pool[chain].txs
     let blocks = pool[chain].blockNumbers
-    urls = [...urls,...adresses.map(e => chain + "-stage.dexguru.biz/address/" + e)]
-    urls = [...urls,...txs.map(e => chain + "-stage.dexguru.biz/tx/" + e)]
-    urls = [...urls,...blocks.map(e => chain + "-stage.dexguru.biz/block/" + e)]
+
+    let addressTypesPool = []
+    for(let address in adresses){
+      const response = await getAddressType(adresses[address])
+      // const data = response
+      // console.log("addressTypesPool: ", response)
+      addressTypesPool.push({[response?.address.address]:response?.address.addressType})
+    }
+    console.log("Pool: ", addressTypesPool)
+
+    console.log("WHOLE LENGTH: ", adresses.length + txs.length + blocks.length)
+    console.log("adresses.length: ", adresses.length)
+    console.log("txs.length: ", txs.length)
+    console.log("blocks.length: ", blocks.length, "\n")
+
+
+
+    // urls = [...urls,...adresses.map(e => chain + "-stage.dexguru.biz/address/" + e)]
+    // urls = [...urls,...txs.map(e => chain + "-stage.dexguru.biz/tx/" + e)]
+    // urls = [...urls,...blocks.map(e => chain + "-stage.dexguru.biz/block/" + e)]
+    urls = [...urls,...adresses.map(e => chain + ".dex.guru/address/" + e)]
+    urls = [...urls,...txs.map(e => chain + ".dex.guru/tx/" + e)]
+    urls = [...urls,...blocks.map(e => chain + ".dex.guru/block/" + e)]
   }
 
   for(let url in urls){
     const fileAddress = './networks/' + urls[url].split(".")[0] + ".csv"
-    console.log("Url: ", urls[url].split(".")[0])
+    // console.log("Url: ", urls[url].split(".")[0])
       fs.appendFile(fileAddress, "https://" + urls[url] + "\n", err => {
           if (err) {
               console.error(err);
